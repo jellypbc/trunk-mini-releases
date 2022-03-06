9f53e479fb282adca96b4fe4a00baee04aa2f75a
cindy <ciindy.wu@gmail.com>
2022-03-05 14:33:32 -1000

    upload files to supabase

--- src/datamodel/supabase/file.ts
+++ src/datamodel/supabase/file.ts
@@ -1,3 +1,19 @@
-export default function uploadFileToSupabase(file: File, sourceURL: string){
+import { supabase } from '../../lib/supabase-client'
+import { DEFAULT_SOURCE_FILES_BUCKET } from '../../lib/constants'
+
+export default async function uploadFileToSupabase(file: File, sourceURL: string){
   console.log('uploading file to supabase...', file, sourceURL)
+  try {
+    let { error : uploadError } = await supabase.storage
+    .from(DEFAULT_SOURCE_FILES_BUCKET)
+    .upload(sourceURL, file)
+
+    if (uploadError){
+      throw uploadError
+    }
+  } catch (error : any) {
+    alert(error.message)
+  } finally {
+    console.log(`uploaded file ${sourceURL} to supabase`)
+  }
 }
