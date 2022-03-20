9d2a9c23aac8c1ddb104b3842a53e653c73d37fe
Denny Luan <dennyluan@gmail.com>
2022-03-19 22:24:26 -1000

    Adds window menu for quit

--- src/frontend/log-in.tsx
+++ src/frontend/log-in.tsx
@@ -6,12 +6,13 @@ import { supabase } from '../lib/supabase-client'
 import { LOCAL_STORAGE_REDIRECT_URL_KEY } from '../lib/constants'
 
 export default function LogIn() {
+  const web = false
 
   async function signInWithGoogle() {
     const redirectUrl = location.href
     localStorage.setItem(LOCAL_STORAGE_REDIRECT_URL_KEY, redirectUrl)
     try {
-      const { error } : { error: any} = await supabase.auth.signIn({
+      const { error } : { error: any } = await supabase.auth.signIn({
         provider: 'google',
       }, {
         redirectTo: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : null || undefined
@@ -39,14 +40,29 @@ export default function LogIn() {
         <p className={styles.description}>
           A reading partner
         </p>
+
+        <div className="btn-group">
+          {
+            web &&
+            <button
+              className={`${styles.magicLinkBtn} btn btn-primary btn-lg mx-3`}
+              onClick={(e) => {
+              e.preventDefault()
+            }}>
+              Download for Mac
+            </button>
+          }
+
           <button
-            className={`${styles.magicLinkBtn} btn btn-primary`}
+            className={`${styles.magicLinkBtn} btn btn-light btn-lg `}
             onClick={(e) => {
             e.preventDefault()
             signInWithGoogle()
           }}>
-            LOGIN WITH GOOGLE
+            Login
           </button>
+        </div>
+
       </main>
       <div className={styles.secret}>
         <Link href="/workspace">
