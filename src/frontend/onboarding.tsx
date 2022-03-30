4ab6805431f0992f29b7da01b44dbfcb7530b3f9
Cindy Wu <ciindy.wu@gmail.com>
2022-03-29 20:31:40 -1000

    adds onboarding page and invite modal (#24)

---
+++ src/frontend/onboarding.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react'
+import styles from './onboarding.module.css'
+import { HotKeys } from 'react-hotkeys'
+import { supabase } from 'src/lib/supabase-client'
+import InviteModal from './invite-modal'
+import { nanoid } from 'nanoid'
+import { useRouter } from 'next/router'
+
+export default function Onboarding({ session } : any) {
+  const [room, setRoom] = useState<string>('')
+  const [showRoomSelector, setShowRoomSelector] = useState<boolean>(false)
+
+  const router = useRouter()
+
+  const { user } = session
+
+  function selectRoom(){
+    let selectedRoom = room
+    if (!selectedRoom) {
+      selectedRoom = nanoid(6)
+    }
+    session && session.user && router.push(`/d/${selectedRoom}`)
+    setRoom('')
+  }
+
+  async function logOut() {
+    const { error } = await supabase.auth.signOut()
+    error ?
+      console.log('Error logging out:', error.message)
+      :
+      alert('You have been signed out')
+  }
+
+  const handlers = {
+    changeCommandBar: () => {
+      alert('Under construction...')
+    }
+  }
+
+  return (
+    <HotKeys
+      {...{
+        style: { outline: "none", display: "flex", flex: 1 },
+        keyMap,
+        handlers,
+      }}
+    >
+      {showRoomSelector &&
+        <InviteModal
+          room={room}
+          setRoom={setRoom}
+          handleSelectRoom={selectRoom}
+          setShowRoomSelector={setShowRoomSelector}
+        />
+      }
+      <div className={styles.container}>
+        <div className={styles.nav}>
+          <input className={styles.search} placeholder={'Search or type ⌘ + K'}>
+          </input>
+          <div
+            className={styles.user}
+            onClick={logOut}
+          >
+            {user.email}
+          </div>
+        </div>
+        <div className={styles.options}>
+          <div className={styles.personalTrunk}>
+            <div className={styles.welcome}>
+              <div className={styles.mainTitle}>
+                Welcome to your Trunk
+              </div>
+              <div className={styles.personalTrunk}>
+                <div className={styles.avatar}>
+                </div>
+                <div className={styles.clientInfo}>
+                  dluan's trunk
+                </div>
+              </div>
+            </div>
+            <div className={styles.personalTrunkActions}>
+              <div>Upload Item</div>
+              or
+              <div>Create a note</div>
+            </div>
+          </div>
+          <div className={styles.otherOptions}>
+            <div className={styles.browsePublicTrunks}>
+              <div className={styles.title}>
+                Browse public trunks
+              </div>
+              <div className={styles.description}>
+                Search for a keyword
+              </div>
+            </div>
+            <div className={styles.joinByInviteCode}>
+              <div className={styles.title}>
+                Have an invite code?
+              </div>
+              <button
+                className={'button button-primary'}
+                onClick={() => setShowRoomSelector(true)}
+              >
+                Join by invite code
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </HotKeys>
+  )
+}
+
+const keyMap = {
+  changeCommandBar: ['command+k']
+}
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
 import type { AuthSession } from '@supabase/supabase-js'
 import { supabase } from '../lib/supabase-client'
 import {
   LOCAL_STORAGE_AUTH_TOKEN_KEY,
-  LOCAL_STORAGE_REDIRECT_URL_KEY
 } from '../lib/constants'
-import { nanoid } from 'nanoid'
-import RoomSelector from '../frontend/room-selector'
+import Onboarding from '../frontend/onboarding'
 import LogIn from '../frontend/log-in'
 import AppNav from './../frontend/nav/app-nav'
 
 export default function Page() {
   const [session, setSession] = useState<AuthSession | null>(null)
-  const [room, setRoom] = useState<string>('')
-  const [showRoomSelector, setShowRoomSelector] = useState<boolean>(false)
-  const router = useRouter()
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((_event: string, session: AuthSession | null) => {
@@ -25,37 +19,12 @@ export default function Page() {
     session && setSession(JSON.parse(session).currentSession)
   }, [])
 
-  useEffect(() => {
-    const redirectUrl = localStorage.getItem(LOCAL_STORAGE_REDIRECT_URL_KEY)
-    let room = redirectUrl && redirectUrl.split('/')[4]
-    room && setRoom(room)
-
-    if (session !== null) {
-      setShowRoomSelector(true)
-    } else {
-      setShowRoomSelector(false)
-    }
-
-  },[session])
-
-  function selectRoom(){
-    let selectedRoom = room
-    if (!selectedRoom) {
-      selectedRoom = nanoid(6)
-    }
-    session && session.user && router.push(`/d/${selectedRoom}`)
-    setRoom('')
-  }
-
   return (
     <>
       <AppNav />
-      {showRoomSelector ?
-        <RoomSelector
-          room={room}
-          setRoom={setRoom}
-          handleSelectRoom={selectRoom}
-          setShowRoomSelector={setShowRoomSelector}
+      {session ?
+        <Onboarding
+          session={session}
         />
         :
         <LogIn/>
