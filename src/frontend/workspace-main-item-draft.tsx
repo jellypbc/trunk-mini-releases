70a3a2837b47f4b55c3156976c5ee8b575c26305
Cindy Wu <ciindy.wu@gmail.com>
2022-04-25 17:06:41 -1000

    client can drag and drop a PDF into the browser (#87)

--- src/frontend/workspace-main-item-draft.tsx
+++ src/frontend/workspace-main-item-draft.tsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import styles from './workspace-main-item-draft.module.css'
 
 export default function MainItemDraft() {
-  return (
-    <div>
-      ItemDraft
+
+  let dropArea = document.getElementById('drop-area');
+
+  ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea && dropArea.addEventListener(eventName, preventDefaults, false)
+  });
+
+  dropArea && dropArea.addEventListener('drop', handleDrop, false)
+
+  function handleDrop(e:any) {
+    let dt = e.dataTransfer
+    let files = dt.files
+
+    handleFiles(files)
+  }
+
+function handleFiles(files :any) {
+  ([...files]).forEach(uploadFile)
+}
+
+function uploadFile(file :any) {
+  console.log('file', file)
+  // let formData = new FormData()
+
+  // formData.append('file', file)
+
+  // fetch(url, {
+  //   method: 'POST',
+  //   body: formData
+  // })
+  // .then(() => { /* Done. Inform the user */ })
+  // .catch(() => { /* Error. Inform the user */ })
+}
+
+
+
+
+  function preventDefaults (e: any) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+return (
+  <div className={styles.container}>
+    <div id="drop-area" className={styles.dropArea}>
+      Drop a file
+
     </div>
+  </div>
   )
 }
