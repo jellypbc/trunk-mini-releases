c7e5f2d7699fbd00cc897525c446be6efd2ef73e
Denny Luan <dennyluan@gmail.com>
2022-05-23 10:45:02 -1000

    Set up next-pwa and service workers

--- worker/index.ts
+++ worker/index.ts
@@ -1,6 +1,6 @@
 import { Server as BaseServer } from "reps-do";
 export { worker as default } from "reps-do";
-import { mutators, type M } from "../src/datamodel/mutators.js";
+import { mutators, type M } from "../src/datamodel/mutators";
 
 export class Server extends BaseServer<M> {
   constructor(state: DurableObjectState) {
