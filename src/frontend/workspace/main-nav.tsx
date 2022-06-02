e3e8ec12e02b865bf8f68e7c606c27aaf5917f86
cindy <ciindy.wu@gmail.com>
2022-06-01 16:24:38 -1000

    fix: close command bar search when client clicks add new item in dashboard view

--- src/frontend/workspace/main-nav.tsx
+++ src/frontend/workspace/main-nav.tsx
@@ -4,14 +4,20 @@ import styles from './main-nav.module.css'
 type MainNavProps = {
   itemCount: number
   handleSetShowMainItemDraft: (state: boolean) => void
+  handleSetCommandBar: (state: boolean) => void
 }
 
-export default function MainNav({ itemCount, handleSetShowMainItemDraft } : MainNavProps ) {
+export default function MainNav({ itemCount, handleSetShowMainItemDraft, handleSetCommandBar } : MainNavProps ) {
+
+  function handleShowMainItemDraftAndCloseCommandBar() {
+    handleSetShowMainItemDraft(true)
+    handleSetCommandBar(false)
+  }
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <span
-          onClick={() => handleSetShowMainItemDraft(true)}
+          onClick={() => handleShowMainItemDraftAndCloseCommandBar()}
           className={styles.addItemButton}
         >Add new item</span>
       </div>
